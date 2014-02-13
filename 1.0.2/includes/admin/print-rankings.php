f2bee6528ec86eb8a35159a2574a2297f2f6888a
conormccauley <conormccauley@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-13 00:17:31 +0000

    Bug fix with profile photos not displaying correctly
    git-svn-id: https://plugins.svn.wordpress.org/wp-athletics/tags@856786 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ 1.0.2/includes/admin/print-rankings.php
@@ -0,0 +1,248 @@
+<?php
+if ( $this->has_permission_to_manage() ) {
+	global $current_user;
+	$nonce = wp_create_nonce( $this->nonce );
+
+	if( isset( $_GET['print'] ) ) {
+?>
+		<div id="wpa-print-rankings">
+
+			<style>
+				table.dataTable tr.odd { background-color: white; }
+				table.dataTable tr.even { background-color: white; }
+
+				table.dataTable tr { border-bottom: 1px solid gray; }
+				table.dataTable td { border-right: 1px solid gray; }
+				table.dataTable { border: 1px solid gray; }
+				table.display th { text-align: left; }
+			</style>
+
+			<script type="text/javascript">
+			jQuery(document).ready(function() {
+				WPA.isAdminScreen = true;
+
+				jQuery('#adminmenuwrap,#adminmenuback,#wpfooter,#wpadminbar').hide();
+				jQuery('#wpcontent').css('marginLeft', '0px');
+				jQuery('.wp-toolbar').css('paddingTop', '0px');
+
+				var gender = '<?php echo $_GET['gender']; ?>';
+				var event = '<?php echo $_GET['event']; ?>';
+				var type = '<?php echo $_GET['type']; ?>';
+				var age = '<?php echo $_GET['age']; ?>';
+				var period = '<?php echo $_GET['period']; ?>';
+
+				var today = jQuery.datepicker.formatDate('dd M yy', new Date())
+
+				jQuery('#wpaPrintHeaderRight div').html(today);
+
+				// set up ajax
+				WPA.Ajax.setup('<?php echo admin_url( 'admin-ajax.php' ); ?>', '<?php echo $nonce; ?>', '<?php echo WPA_PLUGIN_URL; ?>', '<?php echo $current_user->ID; ?>',  function() {
+					jQuery('#wpaPrintHeaderMain').html(((WPA.getEventName(event) + ' ' + WPA.getAgeCategoryDescription(age)) + ' ' + WPA.getProperty('print_rankings_text').toUpperCase() + ' - ' + WPA.getPeriodDescription(period)).toUpperCase());
+					jQuery('#wpaPrintHeaderSub').html('(' + WPA.getGenderDescription(gender) + ' - ' + WPA.getEventSubTypeDescription(type) + ')');
+
+					WPA.rankingsTable = jQuery('#table-rankings').dataTable({
+						"sDom": 'rt',
+						"bProcessing": true,
+						"oLanguage": {
+							"sEmptyTable": WPA.getProperty('table_no_results'),
+							"sProcessing": WPA.getProperty('table_loading_message')
+						},
+						"bPaginate": false,
+						"aaSorting": [[ 1, "asc" ]],
+						"aoColumns": [{
+							"mData": "time_format",
+							"bVisible": false
+						},{
+							"mData": "time",
+							"bVisible": false
+						},{
+							"mData": "rank",
+							"sWidth": "20px",
+							"bSortable": false,
+							"sClass": "datatable-center"
+						},{
+							"mData": "athlete_name",
+							"sClass": "datatable-bold",
+							"bSortable": false
+						},{
+							"mData": "time",
+							"sClass": "datatable-bold",
+							"mRender": WPA.renderTimeColumn,
+							"bSortable": false
+						},{
+							"mData": "time",
+							"mRender": WPA.renderPaceMilesColumn,
+							"bSortable": false
+						},{
+							"mData": "event_name",
+							"bSortable": false
+						},{
+							"mData": "event_location",
+							"bSortable": false
+						},{
+							"mData": "event_sub_type_id",
+							"mRender" : WPA.renderEventTypeColumn,
+							"bSortable": false
+						},{
+							"mData": "event_date",
+							"bSortable": false
+						}]
+					});
+
+					// perform ajax call to get rankings
+					WPA.getRankingsPersonalBests({
+						ageCategory: age,
+						gender: gender,
+						eventSubTypeId: type,
+						eventDate: period,
+						eventCategoryId: event,
+						rankingDisplay: 'best-athlete-result'
+					}, false, function() {
+						window.print();
+					});
+				});
+			});
+			</script>
+
+			<div id="wpaPrintHeader">
+				<div id="wpaPrintHeaderLeft">
+					<p><?php echo $this->get_property('print_rankings_enter_results_text'); ?></p>
+					<p id="wpaPrintHeaderUrl"><?php echo get_bloginfo('wpurl') ?></p>
+				</div>
+				<div id="wpaPrintHeaderCenter">
+					<div id="wpaPrintHeaderClubName"><?php echo get_option( 'wp-athletics_club_name', 'Your club name' ) ?></div>
+					<div id="wpaPrintHeaderMain"></div>
+					<div id="wpaPrintHeaderSub"></div>
+				</div>
+				<div id="wpaPrintHeaderRight">
+					<div></div>
+				</div>
+				<br style="clear:both;"/>
+			</div>
+
+			<div>
+				<table width="100%" class="display ui-state-default" id="table-rankings">
+					<thead>
+						<tr>
+							<th></th>
+							<th></th>
+							<th>#</th>
+							<th><?php echo $this->get_property('column_athlete_name') ?></th>
+							<th><?php echo $this->get_property('column_time') ?></th>
+							<th><?php echo $this->get_property('column_pace') ?></th>
+							<th><?php echo $this->get_property('column_event_name') ?></th>
+							<th><?php echo $this->get_property('column_event_location') ?></th>
+							<th><?php echo $this->get_property('column_event_type') ?></th>
+							<th><?php echo $this->get_property('column_event_date') ?></th>
+						</tr>
+					</thead>
+					<tbody></tbody>
+				</table>
+			</div>
+
+			<div id="wpaPrintFooter">
+				<div><?php echo $this->get_property('print_rankings_powered_by'); ?></div>
+			</div>
+
+		</div>
+<?php
+	}
+	else {
+?>
+	<script type="text/javascript">
+
+	jQuery(document).ready(function() {
+		WPA.isAdminScreen = true;
+
+		// set up ajax
+		WPA.Ajax.setup('<?php echo admin_url( 'admin-ajax.php' ); ?>', '<?php echo $nonce; ?>', '<?php echo WPA_PLUGIN_URL; ?>', '<?php echo $current_user->ID; ?>',  function() {
+
+			WPA.Admin.printUrl = '<?php echo get_bloginfo('wpurl') . '/wp-admin/admin.php?page=wp-athletics-print-rankings' ?>';
+
+			// common setup function
+			WPA.setupCommon();
+
+			// setup common filters
+			WPA.setupFilters();
+
+			// setup gender filter
+			jQuery("#filterGender").combobox({
+				selectClass: 'filter-highlight',
+				defaultValue: 'B'
+			});
+
+			jQuery('#generateRankingsButton button').button({
+				icons: {
+	              primary: 'ui-icon-print'
+	            }
+			}).click(function() {
+
+				var period = jQuery('#filterPeriod').val();
+				var type = jQuery('#filterType').val();
+				var age = jQuery('#filterAge').val();
+				var event = jQuery('#filterEvent').val();
+				var gender = jQuery('#filterGender').val();
+
+				window.open(WPA.Admin.printUrl + '&print=true&period=' + period + '&type=' + type + '&age=' + age + '&event=' + event + '&gender=' + gender,'_blank')
+			});
+
+		});
+	});
+
+	</script>
+
+	<div>
+		<div class="wpa-admin-title">
+			<h2><?php echo $this->get_property('admin_print_rankings_title'); ?></h2>
+		</div>
+		<br style="clear:both"/>
+		<p><?php echo $this->get_property('admin_print_rankings_description'); ?></p>
+	</div>
+
+	<div class="wpa">
+
+		<div class="wpa-menu">
+
+			<!-- FILTERS -->
+			<div class="wpa-filters ui-corner-all">
+				<div class="filter-ignore-for-pb">
+					<select id="filterEvent">
+					</select>
+				</div>
+
+				<select id="filterPeriod">
+					<option value="all" selected="selected"><?php echo $this->get_property('filter_period_option_all'); ?></option>
+					<option value="this_month"><?php echo $this->get_property('filter_period_option_this_month'); ?></option>
+					<option value="this_year"><?php echo $this->get_property('filter_period_option_this_year'); ?></option>
+				</select>
+
+				<select id="filterType">
+					<option value="all" selected="selected"><?php echo $this->get_property('filter_type_option_all'); ?></option>
+				</select>
+
+				<select id="filterGender">
+					<option value="B"><?php echo $this->get_property('gender_B'); ?></option>
+					<option value="M"><?php echo $this->get_property('gender_M'); ?></option>
+					<option value="F"><?php echo $this->get_property('gender_F'); ?></option>
+				</select>
+
+				<select id="filterAge">
+					<option value="all" selected="selected"><?php echo $this->get_property('filter_age_option_all'); ?></option>
+				</select>
+
+				<div id="generateRankingsButton">
+					<a href="<?php get_bloginfo('wpurl') . '/wp-admin/admin.php?page=wp-athletics-settings' ?>" target="new" style="display:none;"></a>
+					<button>Print</button>
+				</div>
+
+			</div>
+
+			<br style="clear:both"/>
+		</div>
+
+	</div>
+
+<?php
+	}
+}
+?>
---
+++ 1.0.2/includes/lang/wp-athletics-sw.php
@@ -0,0 +1,107 @@
+<?php
+
+return array(
+	// my results - table
+    'garmin_link_text' => 'Explore activity on Garmin Connect',
+	'column_event_date' => 'Date',
+	'column_event_type' => 'Terrain',
+	'column_event_name' => 'Event',
+	'column_athlete_name' => 'Athlete',
+	'column_event_location' => 'Location',
+	'column_category' => 'Distance',
+	'column_time' => 'Time',
+	'column_position' => 'Pos.',
+	'column_garmin' => 'Garmin',
+	'column_athlete_name' => 'Name',
+	'column_age_category' => 'Class',
+	'column_top_10' => 'Top 10',
+	'column_club_rank' => 'Rank',
+
+	// my results - profile
+	'my_profile_display_name_label' => 'Idrottsman Namn',
+	'my_profile_age_class' => 'Age Class',
+	'my_profile_select_age_class' => 'Select Age Class',
+	'my_profile_select_fave_event' => 'Select Event',
+	'my_profile_fave_event' => 'Favourite Event',
+	'my_profile_results_recorded' => 'Results Recorded',
+	'my_profile_select_sub_type' => 'Select Terrain',
+	'my_profile_image_upload_text' => 'Click to upload new profile photo',
+	'my_profile_select_profile_image' => 'Select as profile photo',
+	'my_profile_select_profile_image_title' => 'Select Profile Photo',
+
+	// filters
+	'filter_events_option_all' => 'All Events',
+	'filter_period_option_all' => 'All Time',
+	'filter_period_option_this_month' => 'This Month',
+	'filter_period_option_this_year' => 'This Year',
+	'filter_type_option_all' => 'All Terrains',
+	'filter_age_option_all' => 'All Age Classes',
+	'filter_event_name_input_text' => 'Filter event name',
+	'filter_event_name_cancel_text' => 'Remove event name filter',
+
+	// user profile dialog
+	'user_profile_dialog_title' => 'User Profile',
+
+	// event results dialog
+	'event_results_dialog_title' => 'Event Results',
+
+	// top 10 dialog
+	'top_10_dialog_title' => 'Top 10 [age] [category] Results ([period], [type])',
+
+	// my results - tabs
+	'my_results_main_tab' => 'My Results',
+	'my_results_personal_bests_tab' => 'My Personal Bests',
+
+	// results - tabs
+	'results_main_tab' => 'Results',
+	'results_personal_bests_tab' => 'Personal Bests',
+
+	// wpa search
+	'wpa_search_text' => 'Search athlete or event',
+	'wpa_search_category_event' => 'Events',
+	'wpa_search_category_athlete' => 'Athletes',
+
+	// my_results - add result
+	'add_result_event_name' => 'Event Name',
+	'add_result_event_category' => 'Distance',
+	'add_result_age_class' => 'Age Class',
+	'add_result_location' => 'Location',
+	'add_result_event_date' => 'Date',
+	'add_result_event_position' => 'Position',
+	'add_result_event_time_hours' => 'Hours',
+	'add_result_event_time_minutes' => 'Minutes',
+	'add_result_event_time_seconds' => 'Seconds',
+	'add_result_event_time_milliseconds' => 'Milliseconds',
+	'add_result_garmin_link' => 'Garmin ID',
+	'add_result_select_event' => 'Select Event Category',
+	'add_result_optional' => 'optional',
+	'add_result_event_sub_type' => 'Terrain',
+	'add_result_title' => 'Add Event Result',
+	'edit_result_title' => 'Edit Event Result',
+
+	// results - other
+	'my_results_add_result_button' => 'Add Event Result',
+	'delete_result_text' => 'Delete this result (cannot be undone)',
+	'edit_result_text' => 'Edit this result',
+	'confirm_result_delete_title' => 'Confirm Delete Result',
+	'confirm_result_delete' => 'Are you sure you wish to delete this result? This cannot be undone.',
+
+	// help texts
+	'help_add_result_event_name' => 'The event may already exist in our database, start typing the event name and select if it appears in the drop down menu, otherwise it will be added as a new event.',
+	'help_add_result_garmin_id' => 'If you have the activity recorded on Garmin Connect, copy and paste the activity ID in to this field.<br/><br/>For example if the web address of your activity is <strong>http://connect.garmin.com/activity/317262142</strong>, the ID is <strong>317262142</strong>',
+	'help_add_result_cancel_event' => 'Remove chosen event',
+	'help_column_rank' => 'This column represents the overall club ranking for this result, based on age category and event',
+
+	// datatables
+	'table_no_results' => 'There are no event results to display',
+
+	// errors
+	'ajax_no_permission' => 'You do not have permission to perform this request',
+	'my_results_not_logged_in' => 'You must be logged in to manage your athletic results. Please login or register',
+	'error_problem_creating_event' => 'There was a problem creating the new event, please try again later',
+
+	// labels for buttons etc
+	'delete' => 'Delete'
+);
+
+?>
